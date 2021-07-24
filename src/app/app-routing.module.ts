5fcc780714319255437186324ccb204691f65c7d
MOISES MARTINEZ CID <20183l301006@utcv.edu.mx>
2021-07-24 17:31:02 -0500

    Creacion de la pagina de muestra de regalos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegalosComponent } from './Pages/regalos/regalos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/regalos', pathMatch: 'full' },
+  { path: 'regalos', component: RegalosComponent },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Shared/header/header.component';
 import { FooterComponent } from './Shared/footer/footer.component';
+import { RegalosComponent } from './Pages/regalos/regalos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegalosComponent
   ],
   imports: [
     BrowserModule,
