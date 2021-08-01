2f1c56170fafa6f48df386f9581903d3ede02e83
David <20183l301023@utcv.edu.mx>
2021-08-01 18:13:37 -0500

    Se creó el CRUD de categorías (create, update, delete, list)

--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -1,5 +1,8 @@
+import { Categorias } from './../models/categorias';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const url = 'https://sheltered-scrubland-44821.herokuapp.com/api/';
 
 
 @Injectable({
@@ -7,5 +10,25 @@ import { Injectable } from '@angular/core';
 })
 export class CategoriasService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  list(){
+    return this._http.get(`${url}categorias`);
+  }
+
+  insert(categoria: any){
+    return this._http.post(`${url}categoria`, categoria);
+  }
+
+  update(categorias: any){
+    return this._http.put(`${url}categoria/${categorias.id}`, categorias);
+  }
+
+  delete(id: number){
+    return this._http.delete(`${url}categoria/${id}`);
+  }
+
+  categoria_find(id: number){
+    return this._http.get(`${url}categoria-find/${id}`);
+  }
 }
